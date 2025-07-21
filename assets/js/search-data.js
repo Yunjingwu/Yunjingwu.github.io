605da88cc631604b45e4c789355001955599b673
Yunjingwu <51325590+Yunjingwu@users.noreply.github.com>
2025-07-21 09:29:47 +0000

    Deploying to gh-pages from @ Yunjingwu/Yunjingwu.github.io@8f79e195d7b55500a76b9ba36aeb75dae65ce94e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,13 +412,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-awarded-jwst-cycle4-time-pid-7390",
+          title: 'Awarded JWST cycle4 Time (PID 7390)',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
